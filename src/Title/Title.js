c6766ffec752ce7773a13e90701bef3d0e5305b6
Omer Sut <omersutcom@gmail.com>
2022-06-27 10:27:19 +0300

    delete omitrix

--- src/Title/Title.js
+++ src/Title/Title.js
@@ -3,7 +3,6 @@ import styles from "./Title.module.css"
 const Title = () => {
     return(
         <>
-        <div className={styles.ufo1}></div>
         <div className={styles.ufo2}></div>
         </>
     )
